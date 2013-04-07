18d06540126af41852600555b62bead0f7afe641
SL <VojtaCiml@gmail.com>
2013-04-07 19:45:26 +0200

    sync before experiment

--- js/guiderPlayer.js
+++ js/guiderPlayer.js
@@ -12,20 +12,29 @@ function initPlayer(guiderID){
 		printOutput("Undegined guider ID", outputTypes.ERROR);
 		return false;	
 	}
+	showLoading();
 	getGuiderData(guiderID)
+}
+
+function showLoading(){
+    $('#gl-loading').css("display", "block");			
+}
+
+function hideLoading(){
+    $('#gl-loading').css("display", "none");				
 }
 
 function getGuiderData(guiderID){	
 	$.getJSON(glPathToJSONAPI, function(data) {
 		guiderJSON = data;
-		console.log(guiderJSON);		
-		console.log(data);
 		initStage();
 	});		
 }
 
 function initStage(){
-	console.log(guiderJSON);		
+	initStepBoxes();
+	positionStepBoxes();
+	console.log(guiderJSON.steps[0].externalLink);		
 	var items = [];
 	$.each(this.guiderJSON, function(key, val) {
 	items.push('<li id="' + key + '">' + val + '</li>');
@@ -34,6 +43,24 @@ function initStage(){
 	'class': 'my-new-list',
 	html: items.join('')
 	}).appendTo('body');	
+	hideLoading();
+}
+
+function initStepBoxes() {
+	var stepBoxes= [];
+	for(var i=0;i<guiderJSON.steps.length;i++){
+		console.log("jes"+i);
+		stepBoxes.push('<div id="step-'+i+'" class="gl-playerStepWrapper">' + guiderJSON.steps[i].externalLink + '</div>');
+	}
+	$("#gl-stepsWrapper").append(stepBoxes.join(''));
+	
+}
+
+function positionStepBoxes() {
+	for(var i=0;i<guiderJSON.steps.length;i++){
+		$("#step-"+i).css("left",guiderJSON.steps[i].positionX);
+		$("#step-"+i).css("top",guiderJSON.steps[i].positionY);		
+	}	
 }
 
 function printOutput(message, outputTypes){

32f315fb7c78c6084e1cb33d063c1070ec271359
SL <VojtaCiml@gmail.com>
2013-04-07 22:37:18 +0200

    Ready for services analyze

--- js/gl-services.js
+++ js/gl-services.js
@@ -1,1 +1,24 @@
-// JavaScript Document
+// Decode the link, identifz service and create appropriate window
+function createStepString(externalLink){
+	console.log("Analyzing link"+externalLink);
+}
+
+function createVideoBox(){
+	
+}
+
+function createArticleBox(){
+	
+}
+
+function createMapBox(){
+	
+}
+
+function createImageGaleryBox(){
+	
+}
+
+function createCustomCodeBox(){
+	
+}

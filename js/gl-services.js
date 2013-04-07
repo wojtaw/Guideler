876748b636c30da49b6fc127bdb82e05c0492304
SL <VojtaCiml@gmail.com>
2013-04-07 23:19:38 +0200

    attempt to parse URL

--- js/gl-services.js
+++ js/gl-services.js
@@ -1,6 +1,19 @@
 // Decode the link, identifz service and create appropriate window
 function createStepString(externalLink){
 	console.log("Analyzing link"+externalLink);
+	console.log(externalLink.*([^\.]+)(com|net|org|info|coop|int|co\.uk|org\.uk|ac\.uk|uk));
+}
+
+function getLinkParameterByName(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 function createVideoBox(){

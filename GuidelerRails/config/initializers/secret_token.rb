67b94c48769f3d45f8f97d15adf8c1224da1ecb8
Vojta Ciml <VojtaCiml@gmail.com>
2013-04-30 14:02:34 +0200

    Added rails basics

---
+++ GuidelerRails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guideler::Application.config.secret_token = 'bbd45a339a443c8a519212eab4981acdc17bd5ecf2564a7a046459310982d6003c9cb733d15be7196af25be27c2d177a5a7678da1301f62b431dce0a18b813b2'

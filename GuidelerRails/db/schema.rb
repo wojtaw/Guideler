0facd43fd8438e6016e82492c78e73cfce594c33
Vojta Ciml <VojtaCiml@gmail.com>
2013-05-01 21:29:01 +0200

    This will drive me mad

--- GuidelerRails/db/schema.rb
+++ GuidelerRails/db/schema.rb
@@ -11,6 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130501190312) do
+
+  create_table "guiders", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "steps", :force => true do |t|
+    t.integer  "step_order"
+    t.text     "link"
+    t.text     "question"
+    t.string   "answer1"
+    t.string   "answer2"
+    t.string   "answer3"
+    t.integer  "correct_answer"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
 end

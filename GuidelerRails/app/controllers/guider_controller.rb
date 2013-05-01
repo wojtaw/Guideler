55f2aac587e1cca958c230799ac64967571aec06
Vojta Ciml <VojtaCiml@gmail.com>
2013-05-01 22:28:30 +0200

    discovering secrets of controllers

--- GuidelerRails/app/controllers/guider_controller.rb
+++ GuidelerRails/app/controllers/guider_controller.rb
@@ -1,5 +1,15 @@
 class GuiderController < ApplicationController
   def player
+    guider = params[:guider] == nil ? nil : Guider.find(params[:guider])
 
+  rescue ActiveRecord::RecordNotFound
+    guider_not_found
   end
+
+  private
+
+  def guider_not_found
+    render :inline => 'Guider not found'
+  end
+
 end

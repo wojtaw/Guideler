bfcfc1b55f8f41b0e2bc24826694aa2d5b3250fe
Vojta Ciml <VojtaCiml@gmail.com>
2013-05-02 19:41:12 +0200

    basic parsing done

--- GuidelerRails/app/controllers/guider_controller.rb
+++ GuidelerRails/app/controllers/guider_controller.rb
@@ -14,29 +14,33 @@ class GuiderController < ApplicationController
   def guiderJSON
     guider = Guider.find(params[:guiderID])
 
-    uri = URI.parse("http://www.google.com/url?sa=X&q=http://nashville.broadwayworld.com/article/Just_in_time_for_Halloween_Circle_Players_does_JEKYLL_HYDE_20101013&ct=ga&cad=:s7:f1:v1:d2:i0:lt:e0:p0:t1286988171:&cd=yQoOdKUFTLo&usg=AFQjCNEg2inHF8hXGEvG-TxMQyMx7YGHkA")
-    uri_params = CGI.parse(uri.query)
+
 
     service_type = Array.new
+    external_data = Array.new
 
     output_string = "First nothing"
     guider.steps.each do |single_step|
-      service_type.push single_step.link
+      tmp_service = parse_service(single_step.link)
+      tmp_external_data = parse_external_data(tmp_service, single_step.link)
+
+      service_type.push tmp_service
+      external_data.push tmp_external_data
 
     end
 
     service_type.each do |single_field|
+      output_string.concat(single_field+"\n")
+    end
 
+    external_data.each do |single_field|
       output_string.concat(single_field+"\n")
     end
 
 
-    render :text => output_string
+    #render :text => output_string
 
-    service = "SLIDESLIVE"
-    extenal_data = uri_params['q']
-
-    #render :content_type => 'application/json', :locals => { :guider => guider, :serviceType => uri.host, :externalData => extenal_data}, :layout => false
+    render :content_type => 'application/json', :locals => { :guider => guider, :serviceType => service_type, :externalData => external_data}, :layout => false
 
   rescue ActiveRecord::StatementInvalid
     render :inline => 'Guider invalid'
@@ -47,8 +51,28 @@ class GuiderController < ApplicationController
 
   private
 
-  def parse_service
+  def parse_service(parsing_link)
+      url = URI.parse(parsing_link)
+      host = url.host
+      if host.start_with?('www.')
+        host = host[4..host.length]
+      end
 
-     render
+      if host == "youtube.com"
+        return "YOUTUBE"
+      elsif host == "slideslive.com"
+        return "SLIDESLIVE"
+      elsif host == "en.wikipedia.org"
+        return "WIKIPEDIA"
+      else
+        return "UNKNOWN"
+      end
   end
+
+  #finding either video ID or article ID or whatever data that identifies content that should be loaded
+  def parse_external_data(service, parsing_link)
+
+    return "serviceID"
+  end
+
 end

802b4bda54794cc1ece1153f85368d164d5d9655
Vojta Ciml <VojtaCiml@gmail.com>
2013-05-08 23:43:41 +0200

    Basic buggy editing in JS almost done

--- GuidelerRails/app/assets/javascripts/gl-editor.js
+++ GuidelerRails/app/assets/javascripts/gl-editor.js
@@ -22,19 +22,14 @@ function initEditor(){
 function initEditorListeners() {
     $("#edit-newStepButton").click(addStep);
     $("#edit-saveGuiderButton").click(editPrintJson);
-    /*
-    for(var i=0;i<guiderJSON.steps.length;i++){
-        $("#gl-stepButton-"+i).click(function(e) {
-            var tmpNumber =  parseInt(e.target.id.split('-')[2]);
-            showStep(tmpNumber+1);
-        });
-    }
-    */
+    refreshStepListeners();
 }
 
 function addStep(){
     editSteps.push(createStep("","","","","","",""));
+    saveCurrentStep();
     refreshStepBar();
+    editStep(editSteps.length - 1);
 }
 
 function editPrintJson(e){
@@ -48,17 +43,46 @@ function refreshStepBar(){
     console.log("Refreshing");
     var editStepButtons= [];
     for(var i=0;i < editSteps.length;i++){
-        var stepButtonString = '<div id="gl-stepButton-'+i+'" class="edit-stepGeneralButton">'+i+'</div>';
+        var stepButtonString = '<div id="edit-stepButton-'+i+'" class="edit-stepGeneralButton">'+i+'</div>';
         editStepButtons.push(stepButtonString);
     }
     $("#edit-stepButtonsBar").html(editStepButtons.join(''));
+    refreshStepListeners();
+}
+
+function refreshStepListeners(){
+     for(var i=0;i<editSteps.length;i++){
+     $("#edit-stepButton-"+i).click(function(e) {
+         saveCurrentStep();
+     var tmpNumber =  parseInt(e.target.id.split('-')[2]);
+         editStep(tmpNumber);
+     });
+     }
+}
+
+function saveCurrentStep() {
+    var externalLink = $('#edit-stepLink').val();
+    var questionText = $('#edit-question').val();
+    var answer1 = $('#edit-answer1').val();
+    var answer2 = $('#edit-answer2').val();
+    var answer3 = $('#edit-answer3').val();
+    editSteps[currentEditStep] = createStep(externalLink,questionText, answer1, answer2, answer3);
+}
+
+function editStep(stepIndex){
+    currentEditStep = stepIndex;
+    refreshEditTabs();
 }
 
 function refreshEditTabs() {
-
+    $('#edit-stepLink').val(editSteps[currentEditStep].externalLink);
+    $('#edit-question').val(editSteps[currentEditStep].questionText);
+    $('#edit-answer1').val(editSteps[currentEditStep].answer1);
+    $('#edit-answer2').val(editSteps[currentEditStep].answer2);
+    $('#edit-answer3').val(editSteps[currentEditStep].answer3);
 }
 
-function createStep(externalLink, questionEnabled, questionText, answer1, answer2, answer3, correctAnswer){
+function createStep(externalLink, questionText, answer1, answer2, answer3, correctAnswer, questionEnabled){
     var tmpStep = new Object();
     tmpStep.externalLink = externalLink;
     tmpStep.questionEnabled = questionEnabled;

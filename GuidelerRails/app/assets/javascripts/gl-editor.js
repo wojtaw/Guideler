4931cb4a75fd4e4a3588aeb46ae8a44536a80722
Vojta Ciml <VojtaCiml@gmail.com>
2013-05-09 22:17:44 +0200

    Edit styles finally starting looking good

--- GuidelerRails/app/assets/javascripts/gl-editor.js
+++ GuidelerRails/app/assets/javascripts/gl-editor.js
@@ -71,10 +71,15 @@ function initEditorListeners() {
     $("#edit-newStepButton").click(addStep);
     $("#edit-saveGuiderButton").click(saveEditData);
     $("#edit-questionSwitch").click(questionEnableSwitch);
+    $("#edit-stepLink").blur(linkEntered);
     refreshStepListeners();
 }
 
 //---------OTHER LOGIC
+
+function linkEntered(){
+    lightServiceIcon($("#edit-stepLink").val());
+}
 
 function saveEditData(){
     $("#edit-saveGuiderButton").html("Saving...");
@@ -106,7 +111,7 @@ function refreshStepBar(){
     //TODO
     var stateClass = "edit-step-empty";
     for(var i=0;i < editSteps.length;i++){
-        var stepButtonString = '<div id="edit-stepButton-'+i+'" class="edit-stepGeneralButton '+stateClass+'">'+i+'</div>';
+        var stepButtonString = '<div id="edit-stepButton-'+i+'" class="edit-stepGeneralButton '+stateClass+'"></div>';
         editStepButtons.push(stepButtonString);
 
     }
@@ -127,7 +132,7 @@ function refreshStepListeners(){
 }
 
 function refreshEditTabs() {
-    console.log("PARSIIIIIIIIIIIIIIING "+parseURLHost(editSteps[currentEditStep].externalLink));
+    lightServiceIcon(editSteps[currentEditStep].externalLink);
 
     $('#edit-stepLink').val(editSteps[currentEditStep].externalLink);
     $('#edit-question').val(editSteps[currentEditStep].question);
@@ -135,9 +140,20 @@ function refreshEditTabs() {
     $('#edit-answer2').val(editSteps[currentEditStep].answer2);
     $('#edit-answer3').val(editSteps[currentEditStep].answer3);
     //Decheck radios and check proper one
-    //$('input[name=edit-correctAnswer]').attr('checked', false);
-    console.log("Should be checked number /////////"+editSteps[currentEditStep].correctAnswer);
     $('#edit-radio-answer'+editSteps[currentEditStep].correctAnswer).get(0).checked = true;
+}
+
+function lightServiceIcon(sourceLink){
+    var tmpResult = parseURLHost(sourceLink);
+    console.log("Matching resullllllllllllt"+tmpResult);
+    var servicesArray = new Array("google","youtube","vimeo","flickr","picasa","soundcloud","wordpress","wikipedia","slideslive","slideshare");
+    for(var i=0;i<servicesArray.length;i++){
+        if(servicesArray[i] == tmpResult) $('#edit-service-'+servicesArray[i]+'-light').css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 800);
+        else $('#edit-service-'+servicesArray[i]+'-light').css("visibility","hidden");
+    }
+
+
+
 }
 
 function questionEnableSwitch(){
@@ -180,13 +196,17 @@ function createStep(externalLink, question, answer1, answer2, answer3, correctAn
 
 
 function addStep(){
-    editSteps.push(createStep("","","","","","",true));
+    editSteps.push(createStep("","","","","",1,true));
     saveCurrentStep();
     editStep(editSteps.length - 1);
 }
 
 function saveCurrentStep() {
-    editSteps[currentEditStep].externalLink = $('#edit-stepLink').val();
+    var linkResult = validateAndFixURL($('#edit-stepLink').val())
+    if(linkResult == "false")
+        editSteps[currentEditStep].externalLink = "http://www.guideler.com/";
+    else
+        editSteps[currentEditStep].externalLink = linkResult;
     editSteps[currentEditStep].question = $('#edit-question').val();
     editSteps[currentEditStep].answer1 = $('#edit-answer1').val();
     editSteps[currentEditStep].answer2 = $('#edit-answer2').val();
@@ -204,21 +224,49 @@ function editStep(stepIndex){
 function validateAndFixURL(checkUrl){
     if(checkUrl.indexOf("http://") == 0) return checkUrl
     else if(checkUrl.indexOf("https://") == 0) return checkUrl
-    else {
-        if(checkUrl.indexOf("www") == 0){
-            return ("http://"+checkUrl);
-        }
+    else if(checkUrl.indexOf("www") == 0) return ("http://"+checkUrl);
+    else if(isBrokenURL(checkUrl)) return ("http://"+checkUrl);
+    else return "false";
+}
+
+function isBrokenURL(brokenURL){
+    console.log("BROKEN DOMAIN");
+    var containsEnding = false;
+    var tldArray = new Array(".aero",".biz",".cat",".com",".coop",".edu",".gov",".info",".int",".jobs",".mil",".mobi",".museum",
+        ".name",".net",".org",".travel",".ac",".ad",".ae",".af",".ag",".ai",".al",".am",".an",".ao",".aq",".ar",".as",".at",".au",".aw",
+        ".az",".ba",".bb",".bd",".be",".bf",".bg",".bh",".bi",".bj",".bm",".bn",".bo",".br",".bs",".bt",".bv",".bw",".by",".bz",".ca",
+        ".cc",".cd",".cf",".cg",".ch",".ci",".ck",".cl",".cm",".cn",".co",".cr",".cs",".cu",".cv",".cx",".cy",".cz",".de",".dj",".dk",".dm",
+        ".do",".dz",".ec",".ee",".eg",".eh",".er",".es",".et",".eu",".fi",".fj",".fk",".fm",".fo",".fr",".ga",".gb",".gd",".ge",".gf",".gg",".gh",
+        ".gi",".gl",".gm",".gn",".gp",".gq",".gr",".gs",".gt",".gu",".gw",".gy",".hk",".hm",".hn",".hr",".ht",".hu",".id",".ie",".il",".im",
+        ".in",".io",".iq",".ir",".is",".it",".je",".jm",".jo",".jp",".ke",".kg",".kh",".ki",".km",".kn",".kp",".kr",".kw",".ky",".kz",".la",".lb",
+        ".lc",".li",".lk",".lr",".ls",".lt",".lu",".lv",".ly",".ma",".mc",".md",".mg",".mh",".mk",".ml",".mm",".mn",".mo",".mp",".mq",
+        ".mr",".ms",".mt",".mu",".mv",".mw",".mx",".my",".mz",".na",".nc",".ne",".nf",".ng",".ni",".nl",".no",".np",".nr",".nu",
+        ".nz",".om",".pa",".pe",".pf",".pg",".ph",".pk",".pl",".pm",".pn",".pr",".ps",".pt",".pw",".py",".qa",".re",".ro",".ru",".rw",
+        ".sa",".sb",".sc",".sd",".se",".sg",".sh",".si",".sj",".sk",".sl",".sm",".sn",".so",".sr",".st",".su",".sv",".sy",".sz",".tc",".td",".tf",
+        ".tg",".th",".tj",".tk",".tm",".tn",".to",".tp",".tr",".tt",".tv",".tw",".tz",".ua",".ug",".uk",".um",".us",".uy",".uz", ".va",".vc",
+        ".ve",".vg",".vi",".vn",".vu",".wf",".ws",".ye",".yt",".yu",".za",".zm",".zr",".zw");
+    for(var i=0; i < tldArray.length; i++){
+        if(brokenURL.indexOf(tldArray[i]) != -1) containsEnding = true;
     }
+    return containsEnding;
 }
 
 function parseURLHost(urlToParse){
     urlToParse = validateAndFixURL(urlToParse);
-
+    if(urlToParse == "false") return "FALSE";
     console.log("Parsiing -" + urlToParse);
     var a =  document.createElement('a');
     a.href = urlToParse;
     var result = String(a.hostname);
     result = result.toLowerCase();
     if(result.indexOf("www.") === 0) result = result.substring(4,result.length);
-    return result;
+
+    if(result == "youtube.com") return "youtube";
+    else if(result == "vimeo.com") return "vimeo";
+    else if(result == "soundcloud.com") return "soundcloud";
+    else if(result == "flickr.com") return "flickr";
+    else if(result == "wikipedia.com") return "wikipedia";
+    else if(result == "slideslive.com") return "slideslive";
+    else if(result == "slideshare.com") return "slideshare";
+    else return "unknown"
 }
